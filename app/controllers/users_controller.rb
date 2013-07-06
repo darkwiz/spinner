b280d1f1a66aa8215c2ab625b0ecb687f50c7b7c
darkwiz <darkwiz@darkwiz-Dimension-2400.(none)>
2013-07-06 12:47:43 +0200

    hotfix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,6 +53,7 @@ class UsersController < ApplicationController
   # Shows an user profile (and his own spins)
 
   def show
+    @spin = current_user.spins.build if signed_in?
   	@user = User.find(params[:id])
     @spins = @user.spins.page(params[:page]).per(6)
   end
